015578ec7cea414653a68f5c24ff21d7f739b3ee
Guenther Raidl <raidl@ac.tuwien.ac.at>
2016-03-16 11:36:24 +0100

    mingw-std-threads update

--- mingw-std-threads/mingw.mutex.h
+++ mingw-std-threads/mingw.mutex.h
@@ -1,9 +1,20 @@
 /**
 * @file mingw.mutex.h
 * @brief std::mutex et al implementation for MinGW
+** (c) 2013-2016 by Mega Limited, Auckland, New Zealand
+* @author Alexander Vassilev
 *
-* This file is part of the mingw-w64 runtime package.
-* No warranty is given; refer to the file DISCLAIMER within this package.
+* @copyright Simplified (2-clause) BSD License.
+* You should have received a copy of the license along with this
+* program.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* @note
+* This file may become part of the mingw-w64 runtime package. If/when this happens,
+* the appropriate license will be added, i.e. this code will become dual-licensed,
+* and the current BSD 2-clause license will stay.
 */
 
 #ifndef WIN32STDMUTEX_H
@@ -12,7 +23,7 @@
 #if !defined(STDTHREAD_STRICT_NONRECURSIVE_LOCKS) && !defined(NDEBUG)
     #define STDTHREAD_STRICT_NONRECURSIVE_LOCKS
 #endif
-
+#include <windows.h>
 #include <chrono>
 #include <system_error>
 
@@ -30,6 +41,7 @@ public:
         InitializeCriticalSection(&mHandle);
     }
     recursive_mutex (const recursive_mutex&) = delete;
+    recursive_mutex& operator=(const recursive_mutex&) = delete;
     ~recursive_mutex() noexcept
     {
         DeleteCriticalSection(&mHandle);
@@ -64,6 +76,7 @@ public:
 #endif
     {}
     _NonRecursiveMutex (const _NonRecursiveMutex<B>&) = delete;
+    _NonRecursiveMutex& operator= (const _NonRecursiveMutex<B>&) = delete;
     void lock()
     {
         base::lock();
@@ -125,6 +138,7 @@ public:
     typedef HANDLE native_handle_type;
     native_handle_type native_handle() const {return mHandle;}
     recursive_timed_mutex(const recursive_timed_mutex&) = delete;
+    recursive_timed_mutex& operator=(const recursive_timed_mutex&) = delete;
     recursive_timed_mutex(): mHandle(CreateMutex(NULL, FALSE, NULL)){}
     ~recursive_timed_mutex()
     {
@@ -185,6 +199,8 @@ protected:
     typedef _NonRecursiveMutex<recursive_timed_mutex> base;
 public:
     using base::base;
+    timed_mutex(const timed_mutex&) = delete;
+    timed_mutex& operator=(const timed_mutex&) = delete;
     template <class Rep, class Period>
     void try_lock_for(const std::chrono::duration<Rep,Period>& dur)
     {
